55859a5a39987f0fa7fd50482a294b806f5b63d3
Chris Jardine <c_jardine@icloud.com>
2021-11-25 16:36:43 -0500

    Create basic pages and css modules.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,22 @@
 // Step 1: Import React
 import * as React from "react";
 import { Link } from "gatsby";
+
 import Layout from "../components/Layout";
 
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
     <main>
-      <Layout pageTitle="Home">
-        <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="About Me">
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
       </Layout>
     </main>
   );
 };
+
 // Step 3: Export your component
-export default IndexPage;
+export default AboutPage;
